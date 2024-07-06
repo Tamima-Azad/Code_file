350ccdeff055d74f0659dc675ce035858f3bf0aa
Tamima-Azad <tamimaazad368@gmail.com>
2024-07-07 02:32:48 +0600

    added_pithron_code

--- ss.cpp
+++ ss.cpp
@@ -5,13 +5,14 @@ using namespace std;
 int main()
 {
     int n; cin>>n;
-    int ar[n];
+    int a[n];
     for(int i=0;i<n;i++){
-        cin>>ar[i];
+        cin>>a[i];
     }
     for(int i=0;i<n;i++){
-        cout<<ar[i]<<" ";
-    }
+        for(int j=0;j<n;j++){
 
+        }
+    }    
     return 0;
 }
