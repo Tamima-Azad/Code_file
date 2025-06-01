24c76da94a2b1f643c3eb9783ed29ec29ddd32a7
Tamima-Azad <165451442+Tamima-Azad@users.noreply.github.com>
2025-06-01 11:47:42 +0600

    Add files via upload
    1st semester code

---
+++ code/series.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    //if(n=n+5){
+        for(i=0;i<=n;i++){
+            printf("%d",i);
+        }
+        //if(n=n+5)break;
+
+    return 0;
+
+}
